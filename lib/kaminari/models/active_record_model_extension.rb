86e3889893c0914ac062f3be9f634dabc3192e01
esjee <ewfawefw3@gmail.com>
2016-08-06 16:58:55 +0200

    attempt to simplify

--- lib/kaminari/models/active_record_model_extension.rb
+++ lib/kaminari/models/active_record_model_extension.rb
@@ -9,14 +9,17 @@ module Kaminari
 
       # Fetch the values at the specified page number
       #   Model.page(5)
-      eval <<-RUBY
-        def self.#{Kaminari.config.page_method_name}(num = nil)
-          limit(default_per_page).offset(default_per_page * ((num = num.to_i - 1) < 0 ? 0 : num)).extending do
-            include Kaminari::ActiveRecordRelationMethods
-            include Kaminari::PageScopeMethods
-          end
+      def self.page(num = nil)
+        num = num.to_i  # '5' -> 5, and nil -> 0
+        num = 0 if num.negative?
+
+        limit(default_per_page)  # Limit by the number of items we want per page
+          .offset(default_per_page * num)  # Get the items of the page we want
+          .extending do
+          include Kaminari::ActiveRecordRelationMethods
+          include Kaminari::PageScopeMethods
         end
-      RUBY
-    end
+      end  # self.page
+    end  # included
   end
 end
